26ce63d94d6d326ce055a90dd4283c1443069e51
sachin saluja <sachin.saluja132@gmail.com>
2012-05-31 21:30:40 +0530

    testing

--- server.js
+++ server.js
@@ -1,4 +1,28 @@
 var http = require('http');
+var cradle = require('cradle');
+
+var host = process.env["DBHOST"] ? process.env["DBHOST"] : "127.0.0.1";
+var db = new(cradle.Connection)(host).database('tofuapp');
+
+// db.get('123', function (err, doc) {
+//   console.log(doc.title); // 'Darth Vader'
+//   // assert.equal(doc.force, 'dark');
+// });
+
+db.save('skywalker', {
+  force: 'light',
+  name: 'Luke Skywalker'
+}, function (err, res) {
+  if (err) {
+      console.log("error aaya");// Handle error
+  } else {
+      console.log("success hua!!")// Handle success
+  }
+});
+
+
+
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\nApp (tofuapp) is running..');

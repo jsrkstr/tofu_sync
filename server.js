044b17a6f09503d9add0308170017842d28f2b24
sachin saluja <sachin.saluja132@gmail.com>
2012-06-01 15:54:42 +0530

    testing to fix

--- server.js
+++ server.js
@@ -16,23 +16,23 @@ function handler(req, res) {
 
 
 
-io.configure('production', function(){
-	io.enable('browser client minification');  // send minified client
-	io.enable('browser client etag');          // apply etag caching logic based on version number
-	io.enable('browser client gzip');          // gzip the file
-	io.set('log level', 1);                    // reduce logging
-	io.set('transports', [                     // enable all transports (optional if you want flashsocket)
-	    'websocket'
-	  , 'flashsocket'
-	  , 'htmlfile'
-	  , 'xhr-polling'
-	  , 'jsonp-polling'
-	]);
-});
+// io.configure('production', function(){
+// 	io.enable('browser client minification');  // send minified client
+// 	io.enable('browser client etag');          // apply etag caching logic based on version number
+// 	io.enable('browser client gzip');          // gzip the file
+// 	io.set('log level', 1);                    // reduce logging
+// 	io.set('transports', [                     // enable all transports (optional if you want flashsocket)
+// 	    'websocket'
+// 	  , 'flashsocket'
+// 	  , 'htmlfile'
+// 	  , 'xhr-polling'
+// 	  , 'jsonp-polling'
+// 	]);
+// });
 
-io.configure('development', function(){
-	io.set('log level', 1);                    // reduce logging
-});
+// io.configure('development', function(){
+// 	io.set('log level', 1);                    // reduce logging
+// });
 
 
 
